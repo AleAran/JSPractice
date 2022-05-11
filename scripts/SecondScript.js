3df50cc41734bd2c68e8a61d49efa3bd2643b882
merlusamus <alearan93@gmail.com>
2022-05-10 23:52:48 -0300

    Basic logic for store checkout

--- scripts/SecondScript.js
+++ scripts/SecondScript.js
@@ -1,7 +1,39 @@
 // JavaScript source code
 
-//Ph items --> Usar objetos, luego usar array de objetos
-//Elegir items
+//TODO Item Shop & Editor: Won't work on this until the next excercise, since this one doesn't cover object and array... Shame.
+//Ph items --> Array of Objects
+//Item Selection. 
+
+//Add Item
 //Pagar a cuota?
 //Cuantas?
 //Costo total.
+
+
+let mFinalValue = 0;
+let mInstallments;
+let mContinue;
+
+Calculate(parseInt(prompt("Hello User! \nEnter your product value")));
+mContinue = confirm("AddAnotherProduct?");
+
+while (mContinue != false) {
+    Calculate(parseInt(prompt("Enter your product value")));
+    mContinue = confirm("AddAnotherProduct?");
+}
+
+mInstallments = parseInt(prompt("In how many Installments?"));
+Installments(mInstallments);
+
+alert("You have to pay " + mFinalValue + " during " + mInstallments + " Months.");
+
+window.close();
+
+function Calculate(newProductValue) {
+    mFinalValue += newProductValue;
+}
+
+//Barebones Installments calculation without Interest, interest rates will be done later.
+function Installments(totalInstallments) {
+    mFinalValue /= totalInstallments;
+}
