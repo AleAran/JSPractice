8427b417127cb410369d39f02cb417788bcf6271
merlusamus <alearan93@gmail.com>
2022-05-11 00:10:29 -0300

    Added basic rate calculation

--- scripts/SecondScript.js
+++ scripts/SecondScript.js
@@ -4,11 +4,8 @@
 //Ph items --> Array of Objects
 //Item Selection. 
 
-//Add Item
-//Pagar a cuota?
-//Cuantas?
-//Costo total.
 
+const INTEREST_RATE = 5.04;
 
 let mFinalValue = 0;
 let mInstallments;
@@ -33,7 +30,12 @@ function Calculate(newProductValue) {
     mFinalValue += newProductValue;
 }
 
-//Barebones Installments calculation without Interest, interest rates will be done later.
+//Barebones Installments calculation with basic Interest, proper calculation will be done later, once I understand properly how to calculate this with the current laws... this is a pain.
 function Installments(totalInstallments) {
-    mFinalValue /= totalInstallments;
+    //RateCalculation
+    mFinalValue = (mFinalValue * ((totalInstallments * INTEREST_RATE) + 100)) / 100; 
+
+    //We divide the installments and use Math.Round to truncate up to two decimals.
+    mFinalValue = Math.round((mFinalValue /= totalInstallments) * 100) / 100;
+
 }
